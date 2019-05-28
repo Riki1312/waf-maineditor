ba9c191a57423736b9840430c013e232323bb0a2
Riki1312 <riki.brero@gmail.com>
2019-05-28 21:23:37 +0200

    4 files modified

--- src/app/waf-rightpanel/waf-rigthsection-e/waf-rigthsection-e.component.ts
+++ src/app/waf-rightpanel/waf-rigthsection-e/waf-rigthsection-e.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+//
+
+interface PStyle {
+  name: string;
+  propertyCss: string;
+  value?: string;
+}
+
+const StyleProperty_data: PStyle[] = [
+  {
+    name: "Color",
+    propertyCss: "color",
+    value: "#000"
+  },
+  {
+    name: "Opacity",
+    propertyCss: "opacity",
+    value: "1"
+  }
+];
+
 @Component({
   selector: 'app-waf-rigthsection-e',
   templateUrl: './waf-rigthsection-e.component.html',
   styleUrls: ['./waf-rigthsection-e.component.css']
 })
 export class WafRigthsectionEComponent implements OnInit {
+
+  properties: PStyle[] = StyleProperty_data;
 
   constructor() { }
 

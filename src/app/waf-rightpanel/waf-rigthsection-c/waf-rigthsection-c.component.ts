791d5b814d23e2772c6e23f344e67e949f45eeda
Riki1312 <riki.brero@gmail.com>
2019-05-28 20:04:38 +0200

    3 files modified

--- src/app/waf-rightpanel/waf-rigthsection-c/waf-rigthsection-c.component.ts
+++ src/app/waf-rightpanel/waf-rigthsection-c/waf-rigthsection-c.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+//
+
+interface PStyle {
+  name: string;
+  propertyCss: string;
+  value?: string;
+}
+
+const StyleProperty_data: PStyle[] = [
+  {
+    name: "Color",
+    propertyCss: "color",
+    value: "#000"
+  },
+  {
+    name: "Align",
+    propertyCss: "text-align",
+    value: "left"
+  },
+  {
+    name: "Decoration",
+    propertyCss: "text-decoration",
+    value: "none"
+  }
+];
+
 @Component({
   selector: 'app-waf-rigthsection-c',
   templateUrl: './waf-rigthsection-c.component.html',
   styleUrls: ['./waf-rigthsection-c.component.css']
 })
 export class WafRigthsectionCComponent implements OnInit {
+
+  properties: PStyle[] = StyleProperty_data;
 
   constructor() { }
 

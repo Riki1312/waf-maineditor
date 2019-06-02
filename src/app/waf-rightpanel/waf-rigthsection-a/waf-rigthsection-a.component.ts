017a8c222d88916eca35877fbc8cd0671e5c5fd9
Riki1312 <riki.brero@gmail.com>
2019-06-02 22:36:05 +0200

    1 file modified

--- src/app/waf-rightpanel/waf-rigthsection-a/waf-rigthsection-a.component.ts
+++ src/app/waf-rightpanel/waf-rigthsection-a/waf-rigthsection-a.component.ts
@@ -9,57 +9,110 @@ import { WafRightpanelClass, PStyle } from '../waf-rightpanel-class/waf-rightpan
 
 const StyleProperty_data: PStyle[] = [
   {
-    name: "Color",
-    propertyCss: "color",
-    defaultValue: "#000",
-    value: "#000"
+    name: "Display",
+    propertyCss: "display",
+    defaultValue: "block",
+    value: "block",
+    domain: [ "block", "inline", "flex", "grid" ]
   },
   {
-    name: "Align",
-    propertyCss: "text-align",
-    defaultValue: "left",
-    value: "left",
-    domain: [ "left", "right", "center", "justify" ]
+    name: "Flex direction",
+    propertyCss: "flex-direction",
+    defaultValue: "row",
+    value: "row",
+    domain: [ "row", "column" ]
   },
   {
-    name: "Font family",
-    propertyCss: "font-family",
-    defaultValue: "Arial",
-    value: "Arial"
+    name: "Justify content",
+    propertyCss: "justify-content",
+    defaultValue: "flex-start",
+    value: "flex-start",
+    domain: [ "flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly" ]
   },
   {
-    name: "Size",
-    propertyCss: "font-size",
-    defaultValue: "medium",
-    value: "medium"
+    name: "Align items",
+    propertyCss: "align-items",
+    defaultValue: "stretch",
+    value: "stretch",
+    domain: [ "stretch", "flex-start", "flex-end", "center", "baseline" ]
   },
   {
-    name: "Decoration",
-    propertyCss: "text-decoration",
-    defaultValue: "none",
+    name: "Flex grow",
+    propertyCss: "flex-grow",
+    defaultValue: "0",
+    value: "0"
+  },
+  {
+    name: "Align self",
+    propertyCss: "align-self",
+    value: "auto",
+    defaultValue: "auto",
+    domain: [ "flex-start", "flex-end", "center", "stretch", "baseline" ]
+  },
+  {
+    name: "Grid template columns",
+    propertyCss: "grid-template-columns",
+    value: "auto",
+    defaultValue: "auto"
+  },
+  {
+    name: "Grid template rows",
+    propertyCss: "grid-template-rows",
+    value: "auto",
+    defaultValue: "auto"
+  },
+  {
+    name: "Grid gap",
+    propertyCss: "grid-gap",
+    value: "0",
+    defaultValue: "0"
+  },
+  {
+    name: "Justify items",
+    propertyCss: "justify-items",
+    value: "stretch",
+    defaultValue: "stretch",
+    domain: [ "start", "end", "center", "stretch" ]
+  },
+  {
+    name: "Align items",
+    propertyCss: "align-items",
+    value: "start",
+    defaultValue: "start",
+    domain: [ "start", "end", "center", "stretch" ]
+  },
+  {
+    name: "Grid column",
+    propertyCss: "align-items",
+    value: "0 / 0",
+    defaultValue: "0 / 0"
+  },
+  {
+    name: "Grid row",
+    propertyCss: "grid-row",
+    value: "0 / 0",
+    defaultValue: "0 / 0"
+  },
+  {
+    name: "Justify self",
+    propertyCss: "justify-self",
+    value: "start",
+    defaultValue: "start",
+    domain: [ "start", "end", "center", "stretch" ]
+  },
+  {
+    name: "Align self",
+    propertyCss: "align-self",
+    value: "start",
+    defaultValue: "start",
+    domain: [ "start", "end", "center", "stretch" ]
+  },
+  {
+    name: "Float",
+    propertyCss: "float",
     value: "none",
-    domain: [ "underline", "overline", "line-through" ]
-  },
-  {
-    name: "Transform",
-    propertyCss: "text-transform",
-    value: "none",
     defaultValue: "none",
-    domain: [ "capitalize", "uppercase", "lowercase" ]
-  },
-  {
-    name: "Font weight",
-    propertyCss: "font-weight",
-    value: "normal",
-    defaultValue: "normal",
-    domain: [ "bold", "bolder", "lighter" ]
-  },
-  {
-    name: "Style",
-    propertyCss: "font-style",
-    value: "normal",
-    defaultValue: "normal",
-    domain: [ "italic", "oblique" ]
+    domain: [ "none", "left", "right" ]
   }
 ];
 

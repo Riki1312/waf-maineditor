cc610732d8576b259daf7ad2fa42ee90e23e8301
Riki1312 <riki.brero@gmail.com>
2019-06-03 08:52:02 +0200

    2 files modified, 1 added

--- src/app/waf-leftbar/waf-download-code/waf-download-code.component.ts
+++ src/app/waf-leftbar/waf-download-code/waf-download-code.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WafMainService } from '../../waf-services/waf-main.service';
+import { WafDataService } from '../../waf-services/waf-data.service';
+
+//
+
 @Component({
   selector: 'app-waf-download-code',
   templateUrl: './waf-download-code.component.html',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WafDownloadCodeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private MainService: WafMainService, private DataService: WafDataService) { }
 
   ngOnInit() {
+  }
+
+  get wafCode_html() {
+    let htmlCode = `${ this.DataService.GetHtmlCode() }`;
+    return htmlCode;
+  }
+  get wafCode_css() {
+    let cssCode = `${ this.DataService.GetCssCode() }`;
+    return cssCode;
   }
 
 }
